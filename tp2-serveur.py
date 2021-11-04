12324fe2386fc2d340d88798142525d4877dd321
Lemarquand Paul <56430191+Shendraxqt@users.noreply.github.com>
2021-11-04 11:28:57 +0100

    Update tp2-serveur.py

--- tp2-serveur.py
+++ tp2-serveur.py
@@ -1,4 +1,3 @@
-
 import boto3
 import statistics
 
@@ -6,16 +5,19 @@ sqs = boto3.resource('sqs')
 
 queue = sqs.create_queue(QueueName='lab2', Attributes={'DelaySeconds': '0'})
 
-queueReponse = sqs.create_queue(QueueName='lab2.1', Attributes={'DelaySeconds': '0'})
+queueReponse = sqs.create_queue(QueueName='lab2_1', Attributes={'DelaySeconds': '0'})
 
-for message in queue.receive_messages(MaxNumberOfMessages=10):
-    tab=message.body.split()
-    tabnum=list(map(int,tab))
-    tabnum=[0,3,7,5]
-    answer ="moyenne : {}, minimum : {}, maximum : {}, median : {}".format(sum(tabnum)/len(tabnum),min(tabnum),max(tabnum),statistics.median(tabnum))
-    message.delete()
+while(1):
+    answer = ""
+    for message in queue.receive_messages(MaxNumberOfMessages=10):
+        tab=message.body.split()
+        tabnum=list(map(int,tab))
+        answer ="moyenne : {}, minimum : {}, maximum : {}, median : {}".format(sum(tabnum)/len(tabnum),min(tabnum),max(tabnum),statistics.median(tabnum))
+        message.delete()
 
-#send to client
-response=queueReponse.send_message(MessageBody=answer)
+    #send to client
+    if(answer != ""):
+        response=queueReponse.send_message(MessageBody=answer)
 
-#write log file in SC3
+    #write log file in SC3
+    # TODO
