48b9a7a730c1e5a3b497558113dd93952f1302fb
Lemarquand Paul <56430191+Shendraxqt@users.noreply.github.com>
2021-11-04 12:17:59 +0100

    Update tp2-serveur.py

--- tp2-serveur.py
+++ tp2-serveur.py
@@ -2,10 +2,13 @@ import boto3
 import statistics
 
 sqs = boto3.resource('sqs')
+s3 = boto3.resource('s3')
 
 queue = sqs.create_queue(QueueName='lab2', Attributes={'DelaySeconds': '0'})
 
 queueReponse = sqs.create_queue(QueueName='lab2_1', Attributes={'DelaySeconds': '0'})
+
+
 
 while(1):
     answer = ""
@@ -20,5 +23,10 @@ while(1):
         response=queueReponse.send_message(MessageBody=answer)
 
         #write log file in S3
-            with open('logs.txt', 'a') as f:
-                f.write(answer +'\n')
+        with open('logs.txt', 'a') as f:
+            for element in tab:
+                f.write(element + " ")
+            f.write(answer +'\n')
+        BUCKET = "lab2-cc-logs"
+        s3.Bucket(BUCKET).upload_file("logs.txt", "dump/logs.txt")
+
