64acd00980aa0b1113051721921ee4bf6f374a99
MaximeFeuillet <85481775+MaximeFeuillet@users.noreply.github.com>
2021-11-04 10:57:32 +0100

    Update tp2-serveur.py

--- tp2-serveur.py
+++ tp2-serveur.py
@@ -1,3 +1,4 @@
+
 import boto3
 import statistics
 
@@ -5,14 +6,16 @@ sqs = boto3.resource('sqs')
 
 queue = sqs.create_queue(QueueName='lab2', Attributes={'DelaySeconds': '0'})
 
+queueReponse = sqs.create_queue(QueueName='lab2.1', Attributes={'DelaySeconds': '0'})
+
 for message in queue.receive_messages(MaxNumberOfMessages=10):
     tab=message.body.split()
     tabnum=list(map(int,tab))
     tabnum=[0,3,7,5]
     answer ="moyenne : {}, minimum : {}, maximum : {}, median : {}".format(sum(tabnum)/len(tabnum),min(tabnum),max(tabnum),statistics.median(tabnum))
     message.delete()
-print(answer)
 
 #send to client
+response=queueReponse.send_message(MessageBody=answer)
 
 #write log file in SC3
